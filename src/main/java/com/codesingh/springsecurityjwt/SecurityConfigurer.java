133fd3b93984a0415261761d22acfa36d1d3efb8
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-27 23:08:38 -0400

    overridden configure method to free authenticate end point

--- src/main/java/com/codesingh/springsecurityjwt/SecurityConfigurer.java
+++ src/main/java/com/codesingh/springsecurityjwt/SecurityConfigurer.java
@@ -4,6 +4,7 @@ import com.codesingh.springsecurityjwt.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +20,13 @@ public class SecurityConfigurer  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated();
     }
 
     @Bean
