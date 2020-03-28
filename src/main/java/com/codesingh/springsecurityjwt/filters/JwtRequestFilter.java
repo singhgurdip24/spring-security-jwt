59a15a8c6dfa2fba7ae631015b3030aede51d980
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-28 00:14:30 -0400

    fully functional spring security application

--- src/main/java/com/codesingh/springsecurityjwt/filters/JwtRequestFilter.java
+++ src/main/java/com/codesingh/springsecurityjwt/filters/JwtRequestFilter.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -15,10 +16,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class JwtRequestFilter extends OncePerRequestFilter {
-
-    @Autowired
-    public UserDetails userDetails;
 
     @Autowired
     private MyUserDetailsService userDetailsService;
@@ -35,7 +34,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String jwt = null;
 
         if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
-            String jwt = authorizationHeader.substring(7);
+            jwt = authorizationHeader.substring(7);
             username = jwtUtil.extractUsername(jwt);
         }
 
